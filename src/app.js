1a8f8d6e09f95dbc8f0ef07dd53824cb753877ea
Justin Berg <justin.berg1@verizon.com>
2024-09-24 15:46:14 -0500

    finished functionality for my CRUD routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-app.get('/restaurants', async (req, rest) => {
-    const restaurants = await Restaurant.findAll({});
-    rest.json(restaurants);
-})
+//TODO: Create your GET Request Route Below:
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await Restaurant.findAll({});
+  res.json(restaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const number = req.params.id;
+  const restaurant = await Restaurant.findByPk(number);
+  res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const restaurant = await Restaurant.create(req.body);
+  res.json(restaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const updatedRest = await Restaurant.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedRest);
+});
+app.delete("/restaurants/:id", async (req, res) => {
+  const deletedRest = await Restaurant.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deletedRest);
+});
 
 module.exports = app;
